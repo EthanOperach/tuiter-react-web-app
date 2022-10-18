5c1ae788179cac021235e5c69886537b635f749e
EthanOperach <ma2ksethan@gmail.com>
2022-10-18 13:44:52 -0400

    navbar

--- public/tuiter/explore/NavigationSidebar/index.js
+++ public/tuiter/explore/NavigationSidebar/index.js
@@ -2,13 +2,23 @@ const NavigationSidebar = () => {
   return (`
    <div class="list-group">
      <a class="list-group-item" href="/">
-       <i class="fab fa-twitter"></i></a>
+       <i class="fab fa-twitter fa-fw"></i></a>
      <a class="list-group-item" href="/">
-       <i class="fab fa-twitter"></i></a>
+       <i class="fa fa-home fa-fw"></i>Home</a>
      <a class="list-group-item" href="/">
-       <i class="fab fa-twitter"></i></a>
-       <!-- continue rest of list, e.g.,
-            Home, Explore, Notifications,  Messages, etc. -->
+       <i class="fa fa-hashtag fa-fw"></i>Explore</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-bell fa-fw"></i>Notifications</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-envelope fa-fw"></i>Messages</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-bookmark fa-fw"></i>Bookmarks</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-list fa-fw"></i>Lists</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-user fa-fw"></i>Profile</a>
+     <a class="list-group-item" href="/">
+       <i class="fa fa-circle fa-fw"></i>More</a>
       
    </div>
    <div class="d-grid mt-2">
